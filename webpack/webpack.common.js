79ffb93a701f7d79055fa7c57683673602ffea0b
Maurice Wijnia <maurice@van-ons.nl>
2021-11-15 16:10:42 +0100

    [Webpack] Update webpack setup

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,11 +2,11 @@ const webpack = require('webpack');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const config = {
+module.exports = {
     entry: './src/index.ts',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist',
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/dist/',
         filename: 'index.js',
         libraryTarget: 'umd',
         library: {
@@ -14,12 +14,6 @@ const config = {
             amd: 'block-editor-js',
             commonjs: 'block-editor-js',
         },
-    },
-    devServer: {
-        hot: true,
-        compress: true,
-        port: 9001
-        //injectClient: false,
     },
     module: {
         rules: [
@@ -30,26 +24,15 @@ const config = {
             },
             {
                 test: /\.(ts|tsx)$/,
-                loader: 'ts-loader',
+                use: 'ts-loader',
                 exclude: /node_modules/
             },
             {
-                test: /\.css$/,
+                test: /\.(s*)css$/,
                 use: [
-                    MiniCssExtractPlugin.loader,
                     {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 1
-                        }
+                        loader: MiniCssExtractPlugin.loader
                     },
-                    'postcss-loader'
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
                     'css-loader',
                     'sass-loader'
                 ]
@@ -70,12 +53,4 @@ const config = {
     plugins: [
         new MiniCssExtractPlugin({filename: 'styles.css'})
     ]
-};
-
-module.exports = (env, argv) => {
-    if (argv.mode === 'development') {
-        config.devtool = 'eval-cheap-source-map'
-    }
-
-    return config
 };
