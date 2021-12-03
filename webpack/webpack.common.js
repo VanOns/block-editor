997650f519b654a159cdbba89cb41c99ecfa320e
Maurice Wijnia <maurice@van-ons.nl>
2021-12-03 11:57:29 +0100

    [Webpack] Add webpack-dev-server

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -7,6 +7,7 @@ module.exports = {
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, '../dist'),
+        publicPath: '/dist',
         library: {
             name: 'BlockEditor',
             type: 'umd'
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,7 +1,15 @@
+const path = require('path')
 const { merge } = require('webpack-merge')
-const common = require('./webpack.common.js');
+const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
     mode: 'development',
-    devtool: 'eval-cheap-source-map'
+    devtool: 'eval-cheap-source-map',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, '../public')
+        },
+        compress: true,
+        port: 9000
+    }
 })
