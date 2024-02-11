f0ebed1a13ca2c1e5f71dcae287f625087dfd90a
Ferry Stephanus Suwita <ferry.stephanus.s@gmail.com>
2024-02-11 11:22:18 +0700

    feat(playground): add multiple editor

--- playground/main.js
+++ playground/main.js
@@ -1,51 +1,49 @@
 import '../src/styles.scss'
 import * as BlockEditor from '../src/index'
 
-const { hooks } = BlockEditor.wordpress
-
+const {hooks} = BlockEditor.wordpress
 hooks.addFilter('blocks.registerBlockType', 'block-editor', (settings, blockName) => {
-    return settings
+  return settings
 })
 
 document.addEventListener('block-editor/init', e => {
-    console.log(e)
+  console.log('init', e)
 })
 
 const form = document.getElementById('form')
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    console.log('submit', e)
+  e.preventDefault()
+  console.log('submit', e)
 })
 
 const element = document.getElementById('content');
 element.addEventListener('change', (e) => {
-    console.log(e.target.value)
+  console.log('element1 changed', e.target.value)
 })
 
 const settings = {
-    mediaUpload: ({filesList, onFileChange}) => {
-        const files = Array.from(filesList).map(window.URL.createObjectURL)
+  mediaUpload: ({filesList, onFileChange}) => {
+    const files = Array.from(filesList).map(window.URL.createObjectURL)
 
-        onFileChange(files)
+    onFileChange(files)
 
-        setTimeout(() => {
-            const uploadedFiles = Array.from(filesList).map(file => {
-                return {
-                    id: file.name,
-                    name: file.name,
-                    url: `https://dummyimage.com/600x400/000/fff&text=${file.name}`
-                }
-            })
-            onFileChange(uploadedFiles)
-        }, 1000)
-    }
+    setTimeout(() => {
+      const uploadedFiles = Array.from(filesList).map(file => {
+        return {
+          id: file.name,
+          name: file.name,
+          url: `https://dummyimage.com/600x400/000/fff&text=${file.name}`
+        }
+      })
+      onFileChange(uploadedFiles)
+    }, 1000)
+  }
 }
 BlockEditor.initializeEditor(element, settings);
 
 const element2 = document.getElementById('content2');
 element2.addEventListener('change', (e) => {
-    console.log(e.target.value)
+  console.log('element2 changed', e.target.value)
 })
 
 BlockEditor.initializeEditor(element2, settings);
-
